0cc6fb23a8cfedc5f78428e225ba77c940f2e2ec
asbel jar <dreik.elm.mec.game2@gmail.com>
2017-10-13 13:48:09 -0400

    Creacion de los layout

---
+++ app/src/main/java/jhonar/com/elementsofmachine/Preg1Simple.java
@@ -0,0 +1,13 @@
+package jhonar.com.elementsofmachine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Preg1Simple extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preg1_simple);
+    }
+}
